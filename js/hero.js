42337727fa9b38fabd2821f13ad8a09a6735c4b1
tom-oxidise <68251655+tom-oxidise@users.noreply.github.com>
2020-11-02 11:48:20 +0000

    First Push

---
+++ js/hero.js
@@ -0,0 +1,8 @@
+function startSlides() {
+    document.getElementById("slide1").style.opacity = "0";
+    document.getElementById("toggleNav").style.opacity = "1";
+    document.getElementById("slide2").classList.add("active");
+    document.getElementById("2").classList.add("active");
+    document.getElementById("missionCTA").classList.add("active");
+    document.getElementById("toggleNavMob").classList.add("active");
+  }
---
+++ js/slide.js
@@ -0,0 +1,154 @@
+
+    //Mission Toggle//
+    
+    $('#2').click(function() {
+        $('#slide3').removeClass('active');
+    });
+    $('#2').click(function() {
+        $('#slide4').removeClass('active');
+    });
+    $('#2').click(function() {
+        $('#slide5').removeClass('active');
+    });
+    $('#2').click(function() {
+        $('#slide2').addClass('active');
+    });
+    $('#2').click(function() {
+        $('#2').addClass('active');
+    });
+    $('#2').click(function() {
+        $('#3').removeClass('active');
+    });
+    $('#2').click(function() {
+        $('#4').removeClass('active');
+    });
+    $('#2').click(function() {
+        $('#5').removeClass('active');
+    });
+    $('#2').click(function() {
+        $('#missionCTA').addClass('active');
+    });
+    $('#2').click(function() {
+        $('#challengeCTA').removeClass('active');
+    });
+    $('#2').click(function() {
+        $('#solutionsCTA').removeClass('active');
+    });
+    $('#2').click(function() {
+        $('#aboutCTA').removeClass('active');
+    });
+    //Challenges Toggle//
+    
+    $('#3').click(function() {
+        $('#slide2').removeClass('active');
+    });
+    $('#3').click(function() {
+        $('#slide4').removeClass('active');
+    });
+    $('#3').click(function() {
+        $('#slide5').removeClass('active');
+    });
+    $('#3').click(function() {
+        $('#slide3').addClass('active');
+    });
+    $('#3').click(function() {
+        $('#3').addClass('active');
+    });
+    $('#3').click(function() {
+        $('#2').removeClass('active');
+    });
+    $('#3').click(function() {
+        $('#4').removeClass('active');
+    });
+    $('#3').click(function() {
+        $('#5').removeClass('active');
+    });
+    $('#3').click(function() {
+        $('#missionCTA').removeClass('active');
+    });
+    $('#3').click(function() {
+        $('#challengeCTA').addClass('active');
+    });
+    $('#3').click(function() {
+        $('#solutionsCTA').removeClass('active');
+    });
+    $('#3').click(function() {
+        $('#aboutCTA').removeClass('active');
+    });
+     //Services Toggle//
+
+    $('#4').click(function() {
+        $('#slide2').removeClass('active');
+    });
+    $('#4').click(function() {
+        $('#slide3').removeClass('active');
+    });
+    $('#4').click(function() {
+        $('#slide5').removeClass('active');
+    });
+    $('#4').click(function() {
+        $('#slide4').addClass('active');
+    });
+    $('#4').click(function() {
+        $('#4').addClass('active');
+    });
+    $('#4').click(function() {
+        $('#2').removeClass('active');
+    });
+    $('#4').click(function() {
+        $('#3').removeClass('active');
+    });
+    $('#4').click(function() {
+        $('#5').removeClass('active');
+    });
+    $('#4').click(function() {
+        $('#missionCTA').removeClass('active');
+    });
+    $('#4').click(function() {
+        $('#challengeCTA').removeClass('active');
+    });
+    $('#4').click(function() {
+        $('#solutionsCTA').addClass('active');
+    });
+    $('#4').click(function() {
+        $('#aboutCTA').removeClass('active');
+    });
+
+    //About Toggle//
+
+    $('#5').click(function() {
+        $('#slide2').removeClass('active');
+    });
+    $('#5').click(function() {
+        $('#slide3').removeClass('active');
+    });
+    $('#5').click(function() {
+        $('#slide4').removeClass('active');
+    });
+    $('#5').click(function() {
+        $('#slide5').addClass('active');
+    });
+    $('#5').click(function() {
+        $('#5').addClass('active');
+    });
+    $('#5').click(function() {
+        $('#2').removeClass('active');
+    });
+    $('#5').click(function() {
+        $('#3').removeClass('active');
+    });
+    $('#5').click(function() {
+        $('#4').removeClass('active');
+    });
+    $('#5').click(function() {
+        $('#missionCTA').removeClass('active');
+    });
+    $('#5').click(function() {
+        $('#challengeCTA').removeClass('active');
+    });
+    $('#5').click(function() {
+        $('#solutionsCTA').removeClass('active');
+    });
+    $('#5').click(function() {
+        $('#aboutCTA').addClass('active');
+    });
